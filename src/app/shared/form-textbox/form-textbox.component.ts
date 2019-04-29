52887941a80b835510fac1e456664818b8bee23e
Adel <adel.adel@vodafone.com>
2019-04-29 22:28:14 +0200

    Added application structure

---
+++ src/app/shared/form-textbox/form-textbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-textbox',
+  templateUrl: './form-textbox.component.html',
+  styleUrls: ['./form-textbox.component.css']
+})
+export class FormTextboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
